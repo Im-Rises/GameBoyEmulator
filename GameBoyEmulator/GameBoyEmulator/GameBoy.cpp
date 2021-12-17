737c8eb30277fedcab3f17fc3e97921556caf619
Im-Rises <quentin-pimbole@hotmail.fr>
2021-12-17 17:16:22 +0000

    Adding Glew library and starting it

--- GameBoyEmulator/GameBoyEmulator/GameBoy.cpp
+++ GameBoyEmulator/GameBoyEmulator/GameBoy.cpp
@@ -2,7 +2,7 @@
 
 GameBoy* GameBoy::gameboyInstance = 0;
 uint8_t GameBoy::inputs = 0b00111111;
-bool GameBoy::pause=false;
+bool GameBoy::pause = false;
 
 GameBoy::GameBoy() :cpu(&memory)
 {
@@ -68,12 +68,16 @@ void GameBoy::launch()
 	/// <summary>
 	/// OpenGL initialisation
 	/// </summary>
-
 	glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // We want OpenGL 3.3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // We don't want the old OpenGL 
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // We don't want the old OpenGL
+
+	glewExperimental = true; // Needed in core profile
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize GLEW\n");
+		exit(1);
+	}
 
 
 	/// <summary>
