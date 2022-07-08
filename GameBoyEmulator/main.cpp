87a3ff1a7f038f74d385f9b6d2cf54db0837069a
Im-Rises <quentin-morel88@hotmail.com>
2022-07-08 12:32:49 +0200

    wip

--- GameBoyEmulator/main.cpp
+++ GameBoyEmulator/main.cpp
@@ -47,10 +47,10 @@ int main(int argc, char* argv[])
 	if (true) //Debug
 	{
 		// biosPath = "../../../../Bios_Games/Bios/dmg_boot.bin";
-		// romPath = "../../../../Bios_Games/Games/Kirby's dream land.gb";
-		romPath = "../../../../Bios_Games/Games/MarioLand2.gb";
+		romPath = "../../../../Bios_Games/Games/Kirby's dream land.gb";
+		// romPath = "../../../../Bios_Games/Games/MarioLand2.gb";
 		// romPath = "../../../../Bios_Games/Games/bgbtest.gb";
-		//romPath = "../../Bios_Games/Games/Gremlins 2.gb";
+		// romPath = "../../../../Bios_Games/Games/Gremlins 2.gb";
 		// romPath = "../../../../Bios_Games/Games/tetris.gb";
 		// romPath = "../../../../Bios_Games/Games/tennis.gb";
 		// romPath = "../../../../Bios_Games/Games/Zelda Link's Awakening.gb";
@@ -82,7 +82,7 @@ int main(int argc, char* argv[])
 	{
 		gameBoy = GameBoy::getInstance(); //Game Boy creation
 
-		iniLoader.setGameBoyParams(gameBoy);
+		// iniLoader.setGameBoyParams(gameBoy);
 
 		if (argc > 1) //If a game is loaded
 		{
