42c328229b4625301c342861497f8e31721253f4
Im-Rises <quentin-morel88@hotmail.com>
2022-07-13 14:07:16 +0200

    wip

--- GameBoyEmulator/Cartridge.h
+++ GameBoyEmulator/Cartridge.h
@@ -10,34 +10,18 @@ using namespace std;
 
 //p298
 
-enum CartridgeType
-{
-	ROM,
-	MBC1,
-	MBC2,
-	MBC3,
-	MBC5
-};
+// enum CartridgeType
+// {
+// 	ROM,
+// 	MBC1,
+// 	MBC2,
+// 	MBC3,
+// 	MBC5
+// };
 
 class Cartridge
 {
 private:
-	bool cartridgeEmpty;
-	string romPath;
-
-	CartridgeType cartridgeType;
-
-	//Rom and Ram banking
-	uint8* rom = nullptr; //uint8 gameRom[0x100000];//Max rom size 8Mb, 1Mo
-	uint8* ram = nullptr; //Ram bank of size 0x2000 (maximum of 4 ram bank)
-
-	uint8 currentRomBank;
-	uint8 currentRamBank;
-
-	bool romBankingEnable;
-	bool ramBankingEnable;
-
-
 	/*----------------------------Cartridge info---------------------------*/
 	//Other data p299
 	string gameName;
@@ -177,11 +161,27 @@ private:
 	string headerChecksum;
 	string globalChecksum;
 
-	string gameCode;
-	uint8 cgbSupport;
-	uint8 romSize;
-	uint8 externalRamSize;
-	string destinationText;
+
+
+	// bool cartridgeEmpty;
+// string romPath;
+//
+// CartridgeType cartridgeType;
+//
+// //Rom and Ram banking
+// uint8* rom = nullptr; //uint8 gameRom[0x100000];//Max rom size 8Mb, 1Mo
+// uint8* ram = nullptr; //Ram bank of size 0x2000 (maximum of 4 ram bank)
+//
+// uint8 currentRomBank;
+// uint8 currentRamBank;
+//
+// bool romBankingEnable;
+// bool ramBankingEnable;
+	// string gameCode;
+	// uint8 cgbSupport;
+	// uint8 romSize;
+	// uint8 externalRamSize;
+	// string destinationText;
 public
 :
 	/*----------------------------Constructor/Desctructor/Reset---------------------------*/
@@ -189,50 +189,50 @@ public
 	~Cartridge();
 	void reset();
 
-	/*----------------------------Load rom in cartridge---------------------------*/
-	void writeRomInCartridge(const string& romPath);
-
-	/*----------------------------Read and write---------------------------*/
-	uint8 readRom(int address) const;
-	uint8 readRomBank(const uint16& address) const;
-	uint8 readRamBank(const uint16& address) const;
-	void writeRamBank(const uint16& address, const uint8& data);
-	void handleBanking(const uint16& address, const uint8& data);
-
-private
-:
-	/*----------------------------Handle rom and ram banking---------------------------*/
-	void mbcRegister0(const uint16& address, const uint8& data);
-	void mbcRegister1(const uint16& address, const uint8& data);
-	void mbcRegister2(const uint16& address, const uint8& data);
-	void mbcRegister3(const uint16& address, const uint8& data);
-
-public
-:
-	/*----------------------------Savestate---------------------------*/
-	void dump(ofstream& savestateFile);
-	void loadDumpedData(ifstream& savestateFile);
-
-	/*----------------------------Getters and setters---------------------------*/
-	CartridgeType getCartridgeType();
-
-	uint8 getCurrentRamBank() const;
-	void setCurrentRamBank(uint8 value);
-
-	uint8 getCurrentRomBank() const;
-	void setCurrentRomBank(uint8 value);
-
-	bool getRamBankingEnable() const;
-	void setRamBankingEnable(bool state);
-
-	string getCartridgeTypeToString() const;
-	bool getCartridgeIsEmpty() const;
-
-	string getGameName() const;
-	string getRomPath() const;
-
-	/*----------------------------ToString---------------------------*/
-	string toString() const;
-};
+// 	/*----------------------------Load rom in cartridge---------------------------*/
+// 	void writeRomInCartridge(const string& romPath);
+//
+// 	/*----------------------------Read and write---------------------------*/
+// 	uint8 readRom(int address) const;
+// 	uint8 readRomBank(const uint16& address) const;
+// 	uint8 readRamBank(const uint16& address) const;
+// 	void writeRamBank(const uint16& address, const uint8& data);
+// 	void handleBanking(const uint16& address, const uint8& data);
+//
+// private
+// :
+// 	/*----------------------------Handle rom and ram banking---------------------------*/
+// 	void mbcRegister0(const uint16& address, const uint8& data);
+// 	void mbcRegister1(const uint16& address, const uint8& data);
+// 	void mbcRegister2(const uint16& address, const uint8& data);
+// 	void mbcRegister3(const uint16& address, const uint8& data);
+//
+// public
+// :
+// 	/*----------------------------Savestate---------------------------*/
+// 	void dump(ofstream& savestateFile);
+// 	void loadDumpedData(ifstream& savestateFile);
+//
+// 	/*----------------------------Getters and setters---------------------------*/
+// 	CartridgeType getCartridgeType();
+//
+// 	uint8 getCurrentRamBank() const;
+// 	void setCurrentRamBank(uint8 value);
+//
+// 	uint8 getCurrentRomBank() const;
+// 	void setCurrentRomBank(uint8 value);
+//
+// 	bool getRamBankingEnable() const;
+// 	void setRamBankingEnable(bool state);
+//
+// 	string getCartridgeTypeToString() const;
+// 	bool getCartridgeIsEmpty() const;
+//
+// 	string getGameName() const;
+// 	string getRomPath() const;
+//
+// 	/*----------------------------ToString---------------------------*/
+// 	string toString() const;
+// };
 
 #endif
