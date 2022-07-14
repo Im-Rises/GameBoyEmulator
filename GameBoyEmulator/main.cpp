52792a8db3ed838d0abe04c6eabd784505ec3bfd
Im-Rises <quentin-morel88@hotmail.com>
2022-07-14 20:40:07 +0200

    V0.8

--- GameBoyEmulator/main.cpp
+++ GameBoyEmulator/main.cpp
@@ -41,7 +41,7 @@ int main(int argc, char* argv[])
 	string romPath;
 
 
-	if (true) //Debug
+	if (false) //Debug
 	{
 		// biosPath = "../../../../Bios_Games/Bios/dmg_boot.bin";
 		// romPath = "../../../../Bios_Games/Games/Kirby's dream land.gb";
@@ -50,7 +50,7 @@ int main(int argc, char* argv[])
 		// romPath = "../../../../Bios_Games/Games/Gremlins 2.gb";
 		// romPath = "../../../../Bios_Games/Games/tetris.gb";
 		// romPath = "../../../../Bios_Games/Games/tennis.gb";
-		romPath = "../../../../Bios_Games/Games/Zelda Link's Awakening.gb";
+		// romPath = "../../../../Bios_Games/Games/Zelda Link's Awakening.gb";
 		//romPath = "../../Bios_Games/Games/Tennis.gb";
 		// romPath = "../../Bios_Games/Games/Pokemon - Version Bleue (F) [S].gb";
 
@@ -72,7 +72,7 @@ int main(int argc, char* argv[])
 		// romPath = "../../../../Bios_Games/TestRoms/instr_timing/instr_timing.gb";
 		// romPath = "../../../../Bios_Games/TestRoms/mem_timing-2/mem_timing.gb";
 		// romPath = "../../../../Bios_Games/TestRoms/mem_timing/mem_timing.gb";
-		argc = 2;
+		// argc = 2;
 	}
 
 	// if (true || argc > 1)
